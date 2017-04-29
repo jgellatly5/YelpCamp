459ac68349d0bdf9b67bef3a814e36f70049b1d4
Jordan Gellatly <jgellatly5@gmail.com>
2017-04-29 21:34:18 +0000

    set up initial comment flow

--- app.js
+++ app.js
@@ -5,37 +5,10 @@ var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var seedDB = require("./seeds");
 
-seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// Campground.create(
-//     {
-//         name: "Granite Hill",
-//         image: "http://www.dodgeridge.com/wp-content/uploads/2013/08/Eureka_Valley_web.jpg",
-//         description: "This is a huge granite hill. No bathrooms. No water. Beautiful granite!"
-        
-//     }, function(err, campground) {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log("NEWLY CREATED CAMPGROUND: ");
-//             console.log(campground);
-//         }
-//     });
-
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4123/4943676109_b93d9c1203.jpg"},   
-        {name: "Granite Hill", image: "https://farm7.staticflickr.com/6082/6142484013_74e3f473b9.jpg"},   
-        {name: "Mountain Goat's Rest", image: "https://farm6.staticflickr.com/5694/21041875770_ffea6404d0.jpg"}, 
-        // {name: "Salmon Creek", image: "http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},   
-        // {name: "Granite Hill", image: "http://www.dodgeridge.com/wp-content/uploads/2013/08/Eureka_Valley_web.jpg"},   
-        // {name: "Mountain Goat's Rest", image: "http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg"},
-        // {name: "Salmon Creek", image: "http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},   
-        // {name: "Granite Hill", image: "http://www.dodgeridge.com/wp-content/uploads/2013/08/Eureka_Valley_web.jpg"},   
-        // {name: "Mountain Goat's Rest", image: "http://blog.koa.com/wp-content/uploads/unique-campgrounds-626x417.jpg"}
-];
+seedDB();
 
 app.get("/", function(req, res) {
    res.render("landing"); 
@@ -78,10 +51,11 @@ app.get("/campgrounds/new", function(req, res) {
 // SHOW: SHOWS MORE INFO ABOUT ONE CAMPGROUND
 app.get("/campgrounds/:id", function(req, res) {
     // find the campground with provided id
-    Campground.findById(req.params.id, function(err, foundCampground) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
         if (err) {
             console.log(err);
         } else {
+            console.log(foundCampground);
            // render show template with that campground
            res.render("show", {campground: foundCampground});
         }
