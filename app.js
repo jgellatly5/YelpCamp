b42724310baafeb5bc35c603e359f49a356c1521
Jordan Gellatly <jgellatly5@gmail.com>
2017-04-29 23:58:54 +0000

    added new comment page

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("/campgrounds", function(req, res) {
         if(err) {
             console.log(err);
         } else {
-            res.render("index", {campgrounds: allCampgrounds}); 
+            res.render("campgrounds/index", {campgrounds: allCampgrounds}); 
         }
     });
 });
@@ -45,7 +45,7 @@ app.post("/campgrounds", function(req, res) {
 
 // NEW: SHOW FORM TO CREATE NEW CAMPGROUND
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // SHOW: SHOWS MORE INFO ABOUT ONE CAMPGROUND
@@ -57,11 +57,26 @@ app.get("/campgrounds/:id", function(req, res) {
         } else {
             console.log(foundCampground);
            // render show template with that campground
-           res.render("show", {campground: foundCampground});
+           res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+// ===============
+// COMMENTS ROUTE
+// ===============
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
         }
     });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
-   console.log("The YelpCamp server has started!");
+    console.log("The YelpCamp server has started!");
 });
