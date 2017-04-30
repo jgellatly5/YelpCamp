de9920b5a5378e681a19b5950ac6f461c195179b
Jordan Gellatly <jgellatly5@gmail.com>
2017-04-30 04:27:25 +0000

    added registration route and auth

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "It's true what they say... Three's company.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res) {
    res.render("landing"); 
 });
@@ -99,6 +111,28 @@ app.post("/campgrounds/:id/comments", function(req, res) {
                }
             });
         }
+    });
+});
+
+// =============
+// AUTH ROUTES
+// =============
+
+// show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+// handle sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            res.render("/register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/campgrounds"); 
+        });
     });
 });
 
