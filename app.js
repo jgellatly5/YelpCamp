a854ab41a0bddcac21a8a97d7e7ea6cce796a92f
Jordan Gellatly <jgellatly5@gmail.com>
2017-04-30 00:23:22 +0000

    added proper routes for pushing comments to server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 var seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -73,6 +74,26 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
             console.log(err);
         } else {
             res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    // lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+               if(err) {
+                   console.log(err);
+               } else {
+                   campground.comments.push(comment);
+                   campground.save();
+                   res.redirect("/campgrounds/" + campground._id);
+               }
+            });
         }
     });
 });
