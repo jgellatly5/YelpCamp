0b2600cb382771c31bfd7b867522e27435896093
Jordan Gellatly <jgellatly5@gmail.com>
2017-04-30 04:08:40 +0000

    added node packages

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
+var User = require("./models/user");
 var seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
